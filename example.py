c82a7e8fe68045759759440ac458b93fd1f5f8a4
AmadFat <110350824+AmadFat@users.noreply.github.com>
2024-06-16 19:14:55 +0800

    Update example.py

--- example.py
+++ example.py
@@ -8,8 +8,8 @@ data = ['我爱你。',
         '我不恨你。']
 
 prob, lexicon = assembled_pipeline(data)
-print('prob:\\n', prob)
-print('lexicon:\\n', lexicon)
+print('prob:\n', prob)
+print('lexicon:\n', lexicon)
 np.savetxt('analysis_prob.csv', prob, delimiter=',', fmt='%f')
 json_string = json.dumps(lexicon)
 with open('lexicon.json', 'w') as file:
